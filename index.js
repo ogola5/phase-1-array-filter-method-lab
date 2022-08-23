f1b6e6f24046445313f8b2a5f96682b0f7e9150d
ogola5 <ogolaevance5@gmail.com>
2022-08-23 09:40:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+  }
+
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver[0] === attribute[0] })
+  }
+
+  function matchName(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.name === attribute })
+  }
